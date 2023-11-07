f2c4314231df48dcb5a4737a84cd2cd970b96144
jmcmahon01 <joseph.mcmahon2d9@gmail.com>
2023-11-07 12:50:42 -0800

    create form to select ice cream flavor, display result values from array

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+function handleForm(event) {
+  event.preventDefault();
+  const selections = document.querySelectorAll("input:checked");
+  const selectionsArray = Array.from(selections);
+
+  const resultHeading = document.createElement("h2");
+  resultHeading.append("Here are the flavors you selected, you chose......Poorly :(");
+  document.body.append(resultHeading);
+
+  selectionsArray.forEach(function (element) {
+    const paragraph = document.createElement("p");
+    paragraph.append(element.value);
+    document.body.append(paragraph);
+  });
+}
+
+window.addEventListener("load", function () {
+  document.querySelector("form#iceCreamMenu").addEventListener("submit", handleForm);
+});
+
